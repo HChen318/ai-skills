ce9b739c3d5c09ba9262e32f9057793560b01eb2
ch <250001757@qq.com>
2026-09-01 19:35:12 +0800

    chore: update test email prefix to 3world_

--- skills/create-test-account/scripts/fast-create-account.mjs
+++ skills/create-test-account/scripts/fast-create-account.mjs
@@ -37,7 +37,7 @@ const fileBack = ${JSON.stringify(fileBack)}
 const fileAddress = ${JSON.stringify(fileAddress)}
 
 const randomNum = Math.floor(100000 + Math.random() * 900000)
-const email = 'turbo_' + randomNum + '@gmail.com'
+const email = '3world_' + randomNum + '@gmail.com'
 const password = 'Password123!'
 const phone = '9' + Math.floor(1000000 + Math.random() * 9000000).toString().slice(0, 7)
 
